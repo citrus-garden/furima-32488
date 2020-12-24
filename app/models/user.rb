1e87c290cd7aabdc0575f5026c9a3acec9b3159a
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2020-12-24 16:46:31 +0900

    パスワードが英数字混合必須のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true ,uniqueness: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
