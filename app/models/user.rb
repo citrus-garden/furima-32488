c26716afa05fa3dd49680feb7bfe6d9701d15b5c
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2020-12-24 16:39:11 +0900

    お名前が日本語のみのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true ,uniqueness: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
