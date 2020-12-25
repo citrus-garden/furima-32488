7f79c443ae7545f138b1177b0a8c4e2a3588bb12
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2020-12-25 19:04:16 +0900

    CategoryモデルとItemモデルのアソシエーションとバリデーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :infomation
-    validates :category_id
+    validates :category_id, numericality: { other_than: 1 }
     validates :satus_id
     validates :payer_id
     validates :prefecture_id
@@ -11,4 +11,6 @@ class Item < ApplicationRecord
   end
 
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
 end
