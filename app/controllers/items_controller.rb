544a1bb628c7daad57f1df12bf267af46042b51a
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2020-12-25 17:14:01 +0900

    コントローラーにアクションとストロングパラメーターを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,17 @@ class ItemsController < ApplicationController
     def index
         
     end
+
+    def new
+        @item = Item.new
+    end
+
+    def create
+        Item.create(item_params)
+    end
+
+    private
+    def item_params
+        params.require(:item).permit(:name, :infomation, :category_id, :status_id, :payer_id, :prefecture_id, :day_to_delivery_id, :price, :user_id)
+    end
 end
