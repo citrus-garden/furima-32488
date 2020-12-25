82e86ef475dc92e8804e29842629077ffb002b00
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2020-12-25 16:59:01 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :infomation
+    validates :category_id
+    validates :satus_id
+    validates :payer_id
+    validates :prefecture_id
+    validates :day_to_delivery_id	
+    validates :price
+  end
 end
