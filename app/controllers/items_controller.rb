2071e4090d57b85266f2e1c11c5e02e1370dcaf8
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2021-01-05 18:30:30 +0900

    コントローラーにshowアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
         end
     end
 
+    def show
+        @item = Item.find(params[:id])
+    end
+
     private
     def item_params
         params.require(:item).permit(:name, :infomation, :category_id, :status_id, :payer_id, :prefecture_id, :day_to_delivery_id, :price, :user_id, :image).merge(user_id: current_user.id)
