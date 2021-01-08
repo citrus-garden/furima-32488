021cb7ee55fa430531d7be97050f7684e331a7c2
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2021-01-08 14:28:51 +0900

    繰り返し使われているコードを切り出してbefore_actionで実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+    before_action :item_find, only: [:show, :edit, :update]
 
     def index
         @items = Item.all.order("created_at DESC")
@@ -19,18 +20,16 @@ class ItemsController < ApplicationController
     end
 
     def show
-        @item = Item.find(params[:id])
+
     end
 
     def edit
-        @item = Item.find(params[:id])
         if !(current_user.id == @item.user_id)
             redirect_to root_path
         end
     end
 
     def update
-        @item = Item.find(params[:id])
         if @item.update(item_params)
             redirect_to item_path(id: @item.id)
         else
@@ -41,5 +40,9 @@ class ItemsController < ApplicationController
     private
     def item_params
         params.require(:item).permit(:name, :infomation, :category_id, :status_id, :payer_id, :prefecture_id, :day_to_delivery_id, :price, :user_id, :image).merge(user_id: current_user.id)
+    end
+
+    def item_find
+        @item = Item.find(params[:id])
     end
 end
