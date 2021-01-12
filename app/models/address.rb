20262f0da284eb5548d00f6015813151790fce80
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2021-01-12 11:16:40 +0900

    Addressモデルにバリデーションを追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
-    belongs_to :order
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :address
+    validates :phone_number
+  end
+
+  belongs_to :order
 end
