d2b65900b802bea6e620539b62b89d2440553b77
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2021-01-12 11:00:46 +0900

    addressesテーブルのマイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_01_10_021531) do
     t.string "postal_code", null: false
     t.integer "prefecture_id", null: false
     t.string "city", null: false
-    t.string "address"
+    t.string "address", null: false
     t.string "building"
     t.string "phone_number", null: false
     t.bigint "order_id"
