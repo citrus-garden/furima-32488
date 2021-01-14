fdc590ae469e59e022700344345dacbcd9c86b29
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2021-01-14 17:06:18 +0900

    phone_numberに11文字以下でないと保存できないバリデーションを追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -8,7 +8,7 @@ class OrderAddress
         validates :prefecture_id, numericality: { other_than: 1 }
         validates :city
         validates :address
-        validates :phone_number, format: { with: /\A[0-9]+\z/ }
+        validates :phone_number, length: { maximum: 11 }, format: { with: /\A[0-9]+\z/ }
     end
     
     def save
