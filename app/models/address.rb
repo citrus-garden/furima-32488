487607f2a67a227a28e1c15c2a9f777bd2f0b00e
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2021-01-14 11:09:16 +0900

    Formオブジェクトでordersテーブルとaddressesテーブルにデータを保存できるように実装

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,3 @@
 class Address < ApplicationRecord
-  with_options presence: true do
-    validates :postal_code
-    validates :prefecture_id
-    validates :city
-    validates :address
-    validates :phone_number
-  end
-
   belongs_to :order
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :items do
     resources :orders, only: [:index, :create]
   end
-  resources :addresses, only: [:create]
+  # resources :addresses, only: [:create]
 end
