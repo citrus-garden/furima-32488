7876432685b67abe68bfcaa968eaa5986aa57a75
citrus-garden <74897347+citrus-garden@users.noreply.github.com>
2021-01-14 17:13:14 +0900

    「phone_numberが12桁以上だと購入できない」テストコードを追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -89,6 +89,12 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number is invalid")
       end
+
+      it "phone_numberが12桁以上だと購入できないこと" do
+        @order_address.phone_number = "090123456789"
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+      end
     end
   end
 end
